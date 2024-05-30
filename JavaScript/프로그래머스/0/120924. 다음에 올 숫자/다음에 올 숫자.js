940259e144daeaa9343bc0c2b6c858aebd75dd60
Yuhyeon Kim <rladbgus0516@naver.com>
2024-05-30 16:40:58 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,7 @@
+function solution(common) {
+  if (common[1] - common[0] === common.at(-1) - common.at(-2)) {
+    return common.at(-1) + common[1] - common[0];
+  } else {
+    return common.at(-1) * (common[1] / common[0]);
+  }
+}
