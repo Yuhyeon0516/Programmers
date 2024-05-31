73dde0ceabb61f7978c9b55edbd1829e47b13bf2
Yuhyeon Kim <rladbgus0516@naver.com>
2024-05-31 15:32:18 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.95 ms, Memory: 37 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,19 @@
+function solution(arr) {
+  var stk = [];
+  let i = 0;
+
+  while (i < arr.length) {
+    if (!stk.length) {
+      stk.push(arr[i]);
+      i += 1;
+    } else {
+      if (stk.at(-1) < arr[i]) {
+        stk.push(arr[i]);
+        i += 1;
+      } else {
+        stk.pop();
+      }
+    }
+  }
+  return stk;
+}
