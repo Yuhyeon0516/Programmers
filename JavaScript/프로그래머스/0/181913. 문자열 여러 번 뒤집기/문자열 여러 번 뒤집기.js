c307e7d69a054918b889fda1485cccc3b1d9d454
Yuhyeon Kim <rladbgus0516@naver.com>
2024-05-31 15:02:58 +0900

    [level 0] Title: 문자열 여러 번 뒤집기, Time: 8.74 ms, Memory: 34.9 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,13 @@
+function solution(my_string, queries) {
+  for (let query of queries) {
+    my_string =
+      my_string.slice(0, query[0]) +
+      my_string
+        .slice(query[0], query[1] + 1)
+        .split("")
+        .reverse()
+        .join("") +
+      my_string.slice(query[1] + 1);
+  }
+  return my_string;
+}
