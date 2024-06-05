bee9eafc7565933e3322a277d79ec5a07b352f20
Yuhyeon Kim <rladbgus0516@naver.com>
2024-06-05 16:02:52 +0900

    [level 0] Title: 세로 읽기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,7 @@
+function solution(my_string, m, c) {
+  let answer = "";
+  for (let i = 0; i < my_string.length; i += m) {
+    answer += my_string.slice(i, i + m)[c - 1];
+  }
+  return answer;
+}
