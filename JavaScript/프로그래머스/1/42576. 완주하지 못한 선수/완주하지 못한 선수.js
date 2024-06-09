9c30d0e6a70a744469afad30419ea8d3ccdef3b3
Yuhyeon Kim <rladbgus0516@naver.com>
2024-06-09 23:19:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.22 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,9 @@
+function solution(participant, completion) {
+  const sortedParticipant = [...participant].sort();
+  const sortedCompletion = [...completion].sort();
+  for (let i = 0; i < participant.length; i++) {
+    if (sortedParticipant[i] !== sortedCompletion[i])
+      return sortedParticipant[i];
+  }
+  return sortedParticipant.at(-1);
+}
